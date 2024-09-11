a52df618cd6034c42df63f6dbcaf9e2a0688f574
H Jordan <henry@flatfile.io>
2024-09-11 10:56:59 -0500

    update with recommendations and removed unnecessary cruft

--- README.md
+++ README.md
@@ -4,10 +4,9 @@ If you're just getting started with Flatfile, you're in the right place.
 
 This code supports [Flatfile's Beginner Tutorial](https://flatfile.com/docs/quickstart), a four-step walkthrough that helps you create your first data import experience.
 
-1. Configure your data destination: Meet the Workbook
-2. Import and extract your data: Setup a listener
-3. Process your data: Transform and validate data
-4. Export your data: Configure a submit Action
+1. Import and extract your data: Setup a listener
+2. Process your data: Transform and validate data
+3. Export your data: Configure a submit Action
 
 ## Setup your environment variables
 
@@ -20,18 +19,37 @@ FLATFILE_ENVIRONMENT_ID=us_env_1234
 FLATFILE_API_KEY=sk_1234
 ```
 
-## Stepping through the tutorial
+## The concept of Listeners / Agents
 
-After you set up your environment variables, the first step of the tutorial, creating your first Workbook, can be done by running `npm run create-workbook`.
+Your process of using the `initializeFlatfile(config)` will remain consistent - What that wil do:
 
-The remainder of the tutorial can be stepped through in either typescript or javascript.
+- On launch of the embed, a space, workbook, and sheets will be created according to the config you provide
+- As soon as those processes begin, a series of events will be emitted for all application behaviors (i.e. `space:created`, `workbook:created`, `sheet:created`, etc.)
+- This code will handle setting up listeners, and recordHooks - both of which are responsible for listening for specific events - and then performing custom logic accordingly depending on what you need
 
-If using javascript, run `npm run dev:js` to start a local listener.
+Whenever you DEPLOY, we refer to that as deploying an "Agent" - since an agent is going to be the construct responsible for running the code you deploy.
 
-If using typescript, run `npm run dev:ts` to start a local listener.
+## Adding your logic
+
+Essentially, you now have a Typescript ecosystem where you can add your logic. I have added a few examples in typescript/index.ts where you can see some setups. But ultimately, you can then break this into a fully 
+fledged application as things evolve and become more complex.
+
+Once the code is "ready" you can then either run your listeners locally, or deploy them to your environments (see steps below)
+
+## Development / Deployment Recommendations 
+
+hen doing development, you have 2 options
+
+1. Deploy with LOCAL listeners - essentially have the listener code running on your machine while you interact with the embed
+2. Deploy with DEPLOYED listeners - have the listener code running on a server, which is what you'll do when you're ready to deploy
+
+I usually make sure I have 3 ENVIRONMENTS set up in my Dashboard
+
+1. Dev-Local - Here is where I run `npm run dev:ts` to start a local listener
+2. Dev-Deployed - Here is where I run `npm run deploy:ts` to deploy a version of my listener that is running on a server
+3. Prod - Here is where I run `npm run deploy:ts` to deploy a version of my listener that is running on a server when Im confident in my changes
+
+NOTE: The reason for this separation, is you can get some funny business if you run both a local and deployed version of the listener at the same time - since BOTH will be trying to listen for the same events. If you
+ever accidentally deploy to an envionrment by mistake, you can easily remove it via the Dashboard "Agents" page.
 
 Guidance and context for these steps can be found in our [Beginner Tutorial Documentation](https://flatfile.com/docs/quickstart).
-
-## See all code examples
-
-To see all of the code examples from the docs, head to the [flatfile-docs-kitchen-sink](https://github.com/FlatFilers/flatfile-docs-kitchen-sink) repo.
--- typescript/index.ts
+++ typescript/index.ts
@@ -9,196 +9,322 @@ import type { FlatfileEvent, FlatfileListener } from "@flatfile/listener";
 import type { FlatfileRecord } from "@flatfile/plugin-record-hook";
 
 import api from "@flatfile/api";
-import { recordHook } from "@flatfile/plugin-record-hook";
-
-// TODO: Update this with your webhook.site URL for Part 4
-const webhookReceiver = "https://webhook.site/1234";
+import { recordHook, bulkRecordHook } from "@flatfile/plugin-record-hook";
 
 export default function (listener: FlatfileListener) {
+
   // Part 1: Setup a listener (https://flatfile.com/docs/apps/custom/meet-the-listener)
   listener.on("**", (event: FlatfileEvent) => {
-    // Log all events
+    // This is a catch all for all events. Might be a good idea to have this running initially so you can see what events are being emitted
+    // as you interact with Flatfile
     console.log(`Received event: ${event.topic}`);
   });
 
-  listener.namespace(["space:red"], (red: FlatfileListener) => {
-    // Part 2: Configure a Space (https://flatfile.com/docs/apps/custom)
-    red.on(
-      "job:ready",
-      { job: "space:configure" },
-      async (event: FlatfileEvent) => {
-        const { spaceId, environmentId, jobId } = event.context;
-        try {
-          await api.jobs.ack(jobId, {
-            info: "Gettin started.",
-            progress: 10,
-          });
 
-          await api.workbooks.create({
-            spaceId,
-            environmentId,
-            name: "All Data",
-            labels: ["pinned"],
-            sheets: [
-              {
-                name: "Contacts",
-                slug: "contacts",
-                fields: [
-                  {
-                    key: "firstName",
-                    type: "string",
-                    label: "First Name",
-                  },
-                  {
-                    key: "lastName",
-                    type: "string",
-                    label: "Last Name",
-                  },
-                  {
-                    key: "email",
-                    type: "string",
-                    label: "Email",
-                  },
-                ],
-              },
-            ],
-            actions: [
-              {
-                operation: "submitAction",
-                mode: "foreground",
-                label: "Submit foreground",
-                description: "Submit data to webhook.site",
-                primary: true,
-              },
-            ],
-          });
+  /**
+   * This is an example of how you can CREATE / CONFIGURE your space VIA A LISTENER if you want to...
+   * 
+   * Right now, you are simply passing a config into the `initializeFlatfile()` call in the UI. However, sometimes there are reasons for doing things 
+   * server side instead...Some of the main reasons might include:
+   * 
+   * - Maybe you want to dynamically create your config based on some external, async data source
+   * - You want to assign a DEFAULT page assignments - this is done via ID, so the IDs must be created before it can be set
+   * - etc
+   * 
+   * Ultimately, passing in a static config is the easiest way to get setup. But the listener avenue is always an option if you need more complex logic - Ill keep it here  
+   * for reference, but you will not need it if you continue to provide the initial workbook config via the `initializeFlatfile()` call in the UI
+   */
+  // listener.filter({ job: "space:configure" }).on("job:ready", async (event: FlatfileEvent) => {
+  //   const { spaceId, environmentId, jobId } = event.context;
+  //   try {
+  //     await api.jobs.ack(jobId, {
+  //       info: "Gettin started.",
+  //       progress: 10,
+  //     });
 
-          const doc = await api.documents.create(spaceId, {
-            title: "Getting Started",
-            body:
-              "# Welcome\n" +
-              "### Say hello to your first customer Space in the new Flatfile!\n" +
-              "Let's begin by first getting acquainted with what you're seeing in your Space initially.\n" +
-              "---\n",
-          });
+  //     await api.workbooks.create({
+  //       spaceId,
+  //       environmentId,
+  //       name: "All Data",
+  //       labels: ["pinned"],
+  //       sheets: [
+  //         {
+  //           name: "Contacts",
+  //           slug: "contacts",
+  //           fields: [
+  //             {
+  //               key: "firstName",
+  //               type: "string",
+  //               label: "First Name",
+  //             },
+  //             {
+  //               key: "lastName",
+  //               type: "string",
+  //               label: "Last Name",
+  //             },
+  //             {
+  //               key: "email",
+  //               type: "string",
+  //               label: "Email",
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //       actions: [
+  //         {
+  //           operation: "submitAction",
+  //           mode: "foreground",
+  //           label: "Submit foreground",
+  //           description: "Submit data to webhook.site",
+  //           primary: true,
+  //         },
+  //       ],
+  //     });
 
-          await api.spaces.update(spaceId, {
-            environmentId,
-            metadata: {
-              theme: {
-                root: {
-                  primaryColor: "red",
-                },
-                sidebar: {
-                  backgroundColor: "red",
-                  textColor: "white",
-                  activeTextColor: "midnightblue",
-                },
-                // See reference for all possible variables
-              },
-            },
-          });
+  //     const doc = await api.documents.create(spaceId, {
+  //       title: "Getting Started",
+  //       body:
+  //         "# Welcome\n" +
+  //         "### Say hello to your first customer Space in the new Flatfile!\n" +
+  //         "Let's begin by first getting acquainted with what you're seeing in your Space initially.\n" +
+  //         "---\n",
+  //     });
 
-          await api.jobs.complete(jobId, {
-            outcome: {
-              message: "Your Space was created. Let's get started.",
-              acknowledge: true,
-            },
-          });
-        } catch (error) {
-          console.error("Error:", error.stack);
+  //     await api.spaces.update(spaceId, {
+  //       environmentId,
+  //       metadata: {
+  //         theme: {
+  //           root: {
+  //             primaryColor: "red",
+  //           },
+  //           sidebar: {
+  //             backgroundColor: "red",
+  //             textColor: "white",
+  //             activeTextColor: "midnightblue",
+  //           },
+  //           // See reference for all possible variables
+  //         },
+  //       },
+  //     });
 
-          await api.jobs.fail(jobId, {
-            outcome: {
-              message: "Creating a Space encountered an error. See Event Logs.",
-              acknowledge: true,
-            },
-          });
-        }
-      }
-    );
+  //     await api.jobs.complete(jobId, {
+  //       outcome: {
+  //         message: "Your Space was created. Let's get started.",
+  //         acknowledge: true,
+  //       },
+  //     });
+  //   } catch (error) {
+  //     console.error("Error:", error.stack);
 
-    // Part 3: Transform and validate (https://flatfile.com/docs/apps/custom/add-data-transformation)
-    red.use(
-      recordHook("contacts", (record: FlatfileRecord) => {
-        // Validate and transform a Record's first name
-        const value = record.get("firstName");
-        if (typeof value === "string") {
-          record.set("firstName", value.toLowerCase());
-        } else {
-          record.addError("firstName", "Invalid first name");
-        }
+  //     await api.jobs.fail(jobId, {
+  //       outcome: {
+  //         message: "Creating a Space encountered an error. See Event Logs.",
+  //         acknowledge: true,
+  //       },
+  //     });
+  //   }
+  // });
 
-        // Validate a Record's email address
-        const email = record.get("email") as string;
-        const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!validEmailAddress.test(email)) {
-          console.log("Invalid email address");
-          record.addError("email", "Invalid email address");
-        }
-
-        return record;
+  /****************** EXISTING Job Handlers ******************/
+  /**
+   * The UI also has a series of jobs it will run for certain async operations (like mapping) - You can also listen for events for that particular job, and then perform logic based on its outcome
+   * 
+   * The example where this could be valuable is for checking the HEADERS of the mapped file when mapping is complete - and showing a custom message to the user\
+   * 
+   * What we do, is we can listen for a completion of the "workbook:map" job, and then spawn our OWN job using that jobId as CONTEXT, so that we can perform our own syncrhonous logic
+   */
+  listener.on("job:completed", { job: "workbook:map" }, async (event: FlatfileEvent) => {
+    try {
+      // You can add these acknowledgements to acknowledge the work is happening - it also sets the progress bar state in the UI
+      await api.jobs.ack(event.context.jobId, {
+        info: "Gettin started.",
+        progress: 10,
       })
-    );
+      const jobResponse = await api.jobs.get(event.context.jobId)
+      const job = jobResponse.data
+      const sheetId = (job.config as any).sourceSheetId
 
-    // Part 4: Configure a submit Action (https://flatfile.com/docs/apps/custom/submit-action)
-    red.on(
-      "job:ready",
-      { job: "workbook:submitAction" },
-      async (event: FlatfileEvent) => {
-        const { payload } = event;
-        const { jobId, workbookId } = event.context;
+      // Creates a NEW job called "validate-headers" which can then be handled below
+      await api.jobs.create({
+        operation: "validate-headers",
+        input: {
+          jobId: event.context.jobId,
+        },
+        type: "sheet",
+        trigger: "immediate",
+        source: sheetId,
+        managed: true,
+        subject: {
+          type: "collection",
+          query: {},
+          params: {
+            sheetId,
+          },
+          resource: "records",
+        },
+      })
+    } catch (error) {
+      console.log("ERROR", error)
+      await api.jobs.fail(event.context.jobId, {
+        outcome: {
+          message: "This Job failed.",
+        },
+      })
+    }
+  })
 
-        // Acknowledge the job
-        try {
-          await api.jobs.ack(jobId, {
-            info: "Starting job to submit action to webhook.site",
-            progress: 10,
-          });
+  // Handler for the "sheet:validate-headers" job that is spawned above via the completion of any mapping job
+  listener.on("job:ready", { job: "sheet:validate-headers" }, async (event: FlatfileEvent) => {
+    try {
+      await api.jobs.ack(event.context.jobId, {
+        info: "Gettin started.",
+        progress: 10,
+      })
+      // Get data for the current job
+      const jobResponse = await api.jobs.get(event.context.jobId)
+      const job = jobResponse.data
+      const mappingJobId = job.input.jobId
 
-          // Collect all Sheet and Record data from the Workbook
-          const { data: sheets } = await api.sheets.list({ workbookId });
-          const records: { [name: string]: any } = {};
-          for (const [index, element] of sheets.entries()) {
-            records[`Sheet[${index}]`] = await api.records.get(element.id);
-          }
+      // Take the context passed in and use it to get data from the original mapping job
+      const mappingJobResponse = await api.jobs.get(mappingJobId)
+      const mappingJob = mappingJobResponse.data
+      const sheetOfUploadedFile = (mappingJob.config as any).sourceSheetId
+      const sheetOfExpectedSchema = (mappingJob.config as any).destinationSheetId
+      const isValid = false
 
-          console.log(JSON.stringify(records, null, 2));
+      // You now have the schemas/configs for both the originally uploaded file, and the schema that you need customers to match...
+      const { data: configForUploadedFile } = await api.sheets.get({ spaceId: event.context.spaceId, workbookId: event.context.workbookId, sheetId: sheetOfUploadedFile })
+      const { data: configForExpectedSchema } = await api.sheets.get({ spaceId: event.context.spaceId, workbookId: event.context.workbookId, sheetId: sheetOfExpectedSchema })
 
-          // Send the data to our webhook.site URL
-          const response = await fetch(webhookReceiver, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              ...payload,
-              method: "fetch",
-              sheets,
-              records,
-            }),
-          });
+      // perform logic to compare the 2 sheet schemas to find discrepancies between things like field names, types, etc
 
-          if (response.status !== 200) {
-            throw new Error("Failed to submit data to webhook.site");
-          }
 
-          // Otherwise, complete the job
-          await api.jobs.complete(jobId, {
-            outcome: {
-              message: `Data was successfully submitted to Webhook.site. Go check it out at ${webhookReceiver}.`,
-            },
-          });
-        } catch (error) {
-          // If an error is thrown, fail the job
-          console.log(`webhook.site[error]: ${JSON.stringify(error, null, 2)}`);
-          await api.jobs.fail(jobId, {
-            outcome: {
-              message: `This job failed. Check your ${webhookReceiver}.`,
-            },
-          });
-        }
+      // Based on above, you can then show messages/outcomes to the user based on the validity of the uploaded/mapped sheet
+      if (isValid) {
+        await api.jobs.complete(event.context.jobId, {
+          outcome: {
+            message: "Workbook Updated",
+          },
+        })
+      } else {
+        await api.jobs.fail(event.context.jobId, {
+          outcome: {
+            message: "This workbook is not valid. Please check your data.",
+          },
+        })
       }
-    );
-  });
+    } catch (error) {
+      console.log("ERROR", error)
+      await api.jobs.fail(event.context.jobId, {
+        outcome: {
+          message: "This Job failed.",
+        },
+      })
+    }
+  })
+
+  /****************** CUSTOM Job Handlers ******************/
+  /**
+   * Within your Workbook, you can define ACTIONS - These appear as clickable buttons/options on either the WORKBOOK level, or SHEET level depending on where you define them.
+   * 
+   * EXAMPLE:
+   *  {
+   *    ...other_workbook_config,
+   *    "actions": [
+   *      {
+   *        "operation": "validate-accuracy",
+   *        "mode": "foreground",
+   *        "label": "Validate Accuracy",
+   *        "description": "Validate the accuracy of the data",
+   *        "primary": true
+   *      }
+   *    ]
+   *  }
+   * 
+   * Upon INVOKING that action, a JOB will then be created. Jobs are containers for trackable, async operations. Using a listener, you can respond to the creation of a job, and 
+   * perform any logic you want within that context.
+   * 
+   */
+
+  /**
+   * An example that we talked about, could be large, check for sheet validity. While you could possibly do this in recordHook - sometimes it is best for full-sheet validations to be done in a Job
+   */
+  listener.on("job:ready", { job: "workbook:validate-accuracy" }, async ({ context, payload }) => {
+    const { jobId } = context
+
+    try {
+      await api.jobs.ack(jobId, {
+        info: "Gettin started.",
+        progress: 10,
+      })
+
+      const { data: workbook } = await api.workbooks.get({ spaceId: context.spaceId, workbookId: context.workbookId })
+      let isValid = false
+
+
+      // Perform logic here to get records, sheets, etc and check for validity / accuracy
+
+
+      if (isValid) {
+        await api.jobs.complete(jobId, {
+          outcome: {
+            message: "Workbook Updated",
+          },
+        })
+      } else {
+        await api.jobs.fail(jobId, {
+          outcome: {
+            message: "This workbook is not valid. Please check your data.",
+          },
+        })
+      }
+    } catch (error) {
+      console.log("ERROR", error)
+      await api.jobs.fail(jobId, {
+        outcome: {
+          message: "This Job failed.",
+        },
+      })
+    }
+  })
+
+  /****************** Record Hooks ******************/
+
+  /**
+   * This is an example of a recordHook. Record hooks are a type of listener that can be used to perform custom logic on records that are added/updated to a specific 
+   * 
+   * This is good for simple, one-off transformations that run on a record-by-record basis.
+   */
+  listener.use(
+    recordHook("<slug_of_sheet_where_hook_will_run>", async (record: FlatfileRecord) => {
+      let last = record.get("lastName") as string
+
+      if (last !== null) {
+        const newLast = last.toUpperCase()
+        record.set("lastName", newLast)
+      } else {
+        record.addError("lastName", "Must have a last name")
+      }
+
+      return record
+    })
+  )
+
+  /**
+ * This is an example of a bulkRecordHook. It is similar to the recordHook, but it is designed to process multiple records at once.
+ * 
+ * This is good for more complex transformations that need to process multiple records at once.
+ */
+  listener.use(
+    bulkRecordHook("<slug_of_sheet_where_hook_will_run>", (records) => {
+      // Process records here
+      return records.map(record => {
+        // Modify each record
+        return record;
+      });
+    })
+  );
+
+
+
 }
